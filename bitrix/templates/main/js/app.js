7cf5eb1ad17a4dcda7b982ddeecb4cdc7c108079
gitwhatislove <whatislove0505@gmail.com>
2017-02-08 17:23:32 +0300

    'working_main_page'

--- bitrix/templates/main/js/app.js
+++ bitrix/templates/main/js/app.js
@@ -31,6 +31,20 @@ function initSlider() {
 		nextArrow: "<button type='button' class='slider_arrow slider_arrow-next'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 15 27' enable-background='new 0 0 15 27' xml:space='preserve'><path fill-rule='evenodd' clip-rule='evenodd' d='M15,13.7L1.3,27L0,25.7l12.3-12.1L0,1.1L1.2,0L15,13.7z'/></svg></button>",
 	});
 }
+function initSliderVideo() {
+	$('.js-slider-init-video').slick({
+		slidesToShow: 3,
+		slidesToScroll: 2,
+		dots: false,
+		arrows: true,
+		variableWidth: true,
+		infinite: true,
+		prevArrow: "<button type='button' class='slider_arrow slider_arrow-pre'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 15 27' enable-background='new 0 0 15 27' xml:space='preserve'><path fill-rule='evenodd' clip-rule='evenodd' d='M13.8,0L15,1.1L2.7,13.6L15,25.7L13.7,27L0,13.7L13.8,0z'/></svg></button>",
+		nextArrow: "<button type='button' class='slider_arrow slider_arrow-next'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 15 27' enable-background='new 0 0 15 27' xml:space='preserve'><path fill-rule='evenodd' clip-rule='evenodd' d='M15,13.7L1.3,27L0,25.7l12.3-12.1L0,1.1L1.2,0L15,13.7z'/></svg></button>",
+	});
+}
 $( document ).ready(function() {
-	initSlider();
+	if ($('.js-slider-init').length) initSlider();
+	if ($('.js-slider-init-video').length) initSliderVideo();
+	
 });
