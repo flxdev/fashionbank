b7269537bad1846f1f8028a83285a8044b17b76e
gitwhatislove <whatislove0505@gmail.com>
2017-02-10 16:10:38 +0300

    'fix_mobile'

--- bitrix/templates/main/js/app.js
+++ bitrix/templates/main/js/app.js
@@ -29,6 +29,16 @@ function initSlider() {
 		arrows: true,
 		prevArrow: "<button type='button' class='slider_arrow slider_arrow-pre'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 15 27' enable-background='new 0 0 15 27' xml:space='preserve'><path fill-rule='evenodd' clip-rule='evenodd' d='M13.8,0L15,1.1L2.7,13.6L15,25.7L13.7,27L0,13.7L13.8,0z'/></svg></button>",
 		nextArrow: "<button type='button' class='slider_arrow slider_arrow-next'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 15 27' enable-background='new 0 0 15 27' xml:space='preserve'><path fill-rule='evenodd' clip-rule='evenodd' d='M15,13.7L1.3,27L0,25.7l12.3-12.1L0,1.1L1.2,0L15,13.7z'/></svg></button>",
+		responsive: [
+		{
+		 	breakpoint: 1150,
+			settings: {
+				rows: 3,
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		}
+	  ]
 	});
 }
 function initSliderVideo() {
@@ -41,6 +51,15 @@ function initSliderVideo() {
 		infinite: true,
 		prevArrow: "<button type='button' class='slider_arrow slider_arrow-pre'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 15 27' enable-background='new 0 0 15 27' xml:space='preserve'><path fill-rule='evenodd' clip-rule='evenodd' d='M13.8,0L15,1.1L2.7,13.6L15,25.7L13.7,27L0,13.7L13.8,0z'/></svg></button>",
 		nextArrow: "<button type='button' class='slider_arrow slider_arrow-next'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 15 27' enable-background='new 0 0 15 27' xml:space='preserve'><path fill-rule='evenodd' clip-rule='evenodd' d='M15,13.7L1.3,27L0,25.7l12.3-12.1L0,1.1L1.2,0L15,13.7z'/></svg></button>",
+		responsive: [
+		{
+		 	breakpoint: 1150,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		}
+	  ]
 	});
 }
 $( document ).ready(function() {
