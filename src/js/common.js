d3e876a4b0e2d3380879516187acce743147b999
gitwhatislove <whatislove0505@gmail.com>
2017-02-14 10:02:31 +0300

    'mobile_version'

--- src/js/common.js
+++ src/js/common.js
@@ -9,12 +9,15 @@ function initSlider() {
 		nextArrow: "<button type='button' class='slider_arrow slider_arrow-next'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 15 27' enable-background='new 0 0 15 27' xml:space='preserve'><path fill-rule='evenodd' clip-rule='evenodd' d='M15,13.7L1.3,27L0,25.7l12.3-12.1L0,1.1L1.2,0L15,13.7z'/></svg></button>",
 		responsive: [
 		{
-		 	breakpoint: 1150,
+			breakpoint: 1150,
 			settings: {
 				rows: 3,
 				slidesToShow: 2,
 				slidesToScroll: 1
 			}
+		},{
+			breakpoint: 700,
+			settings: 'unslick'
 		}
 	  ]
 	});
@@ -31,17 +34,36 @@ function initSliderVideo() {
 		nextArrow: "<button type='button' class='slider_arrow slider_arrow-next'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 15 27' enable-background='new 0 0 15 27' xml:space='preserve'><path fill-rule='evenodd' clip-rule='evenodd' d='M15,13.7L1.3,27L0,25.7l12.3-12.1L0,1.1L1.2,0L15,13.7z'/></svg></button>",
 		responsive: [
 		{
-		 	breakpoint: 1150,
+			breakpoint: 1150,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 1
 			}
+		},{
+			breakpoint: 700,
+			settings: 'unslick'
 		}
 	  ]
 	});
 }
+
+function shadowScroll(scrollContainer, scrolling, add) {
+	if ($(scrolling).position().left < 0)  $(add).addClass('shadow_left');
+	else $(add).removeClass('shadow_left');
+	if ($(scrolling).position().left + $(scrolling).width() > $(scrollContainer).width()) $(add).addClass('shadow_right');
+	else $(add).removeClass('shadow_right');
+}
 $( document ).ready(function() {
 	if ($('.js-slider-init').length) initSlider();
 	if ($('.js-slider-init-video').length) initSliderVideo();
+	if ($('.js-scroll-content').length) {
+		shadowScroll('.js-scroll-container', '.js-scroll-content', '.js-scroll-opasity');
+		$('.js-scroll-container').on( 'scroll', function(){
+			shadowScroll('.js-scroll-container', '.js-scroll-content', '.js-scroll-opasity');
+		});
+		$(window).on( 'resize', function(){
+			shadowScroll('.js-scroll-container', '.js-scroll-content' , '.js-scroll-opasity');
+		});
+	}
 	
 });
