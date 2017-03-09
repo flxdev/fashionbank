10137c4a705e6ac6abdd433397623e4edcb8cd12
gitwhatislove <whatislove0505@gmail.com>
2017-03-09 16:08:25 +0300

    'add_new_commit'

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 // =include lib/jquery.min.js
 // =include lib/slick.min.js
 // =include lib/jquery-ui.min.js
+// =include lib/perfect-scrollbar.min.js
+// =include lib/perfect-scrollbar.jquery.min.js
 
 // =include common.js
