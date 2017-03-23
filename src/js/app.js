b400af3d4c4e98fb8b1cd9bee6f99d3c453d6025
gitwhatislove <whatislove0505@gmail.com>
2017-03-23 16:04:54 +0300

    'fix_bugs'

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@
 // =include lib/perfect-scrollbar.min.js
 // =include lib/perfect-scrollbar.jquery.min.js
 // =include lib/jquery.inputmask.bundle.js
+// =include lib/jquery.scrollTo.min.js
 
 
 // =include common.js
