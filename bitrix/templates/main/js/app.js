e04f68c864e653db4fba187e6c14f44e6c041f17
Tim <andrei.kurlyandchik@gmail.com>
2017-06-14 11:05:13 +0300

    add 1-12sport

--- bitrix/templates/main/js/app.js
+++ bitrix/templates/main/js/app.js
@@ -3026,8 +3026,8 @@ function initSliderOne() {
 		adaptiveHeight: true,
 		dots: false,
 		arrows: true,
-		prevArrow: "<button type='button' class='slider_arrow slider_arrow-pre'><div class='wrapper-slider-arrow'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 15 27' enable-background='new 0 0 15 27' xml:space='preserve'><path fill-rule='evenodd' clip-rule='evenodd' d='M13.8,0L15,1.1L2.7,13.6L15,25.7L13.7,27L0,13.7L13.8,0z'/></svg></div></button>",
-		nextArrow: "<button type='button' class='slider_arrow slider_arrow-next'><div class='wrapper-slider-arrow'><svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 15 27' enable-background='new 0 0 15 27' xml:space='preserve'><path fill-rule='evenodd' clip-rule='evenodd' d='M15,13.7L1.3,27L0,25.7l12.3-12.1L0,1.1L1.2,0L15,13.7z'/></svg></div></button>",
+		prevArrow: "<button type='button' class='slider_arrow slider_arrow-pre btn-slider-prev'><img src='./img/prev_shadow.png' alt=''></button>",
+		nextArrow: "<button type='button' class='slider_arrow slider_arrow-next btn-slider-next'><img src='./img/next_shadow.png' alt=''></button>",
 	});
 }
 
